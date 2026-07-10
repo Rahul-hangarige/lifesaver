bf12dee8e7a25a8d37988f488520dcade2bdb5ed
Rahul <skrrahul77@gmail.com>
2026-07-11 01:30:50 +0530

    Update CORS settings

--- client/src/context/NotificationContext.jsx
+++ client/src/context/NotificationContext.jsx
@@ -13,7 +13,7 @@ export const NotificationProvider = ({ children }) => {
 
   useEffect(() => {
     if (user) {
-      const newSocket = io('http://localhost:5000');
+      const newSocket = io('https://lifesaver-33e3.onrender.com');
       setSocket(newSocket);
 
       newSocket.emit('join-room', user.id);
